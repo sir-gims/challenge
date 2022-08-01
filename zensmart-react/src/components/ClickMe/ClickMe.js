29fa787aaa93718e30d9cf5e059929a76ead00c2
Agim Bakiu <bakiuagim@gmail.com>
2022-08-02 00:39:33 +0200

    feat: created model and schema

--- zensmart-react/src/components/ClickMe/ClickMe.js
+++ zensmart-react/src/components/ClickMe/ClickMe.js
@@ -2,19 +2,36 @@ import {useState, useEffect} from "react";
 const axios = require('axios').default;
 function ClickMe() {
     let [counter, setCounter] = useState(0);
-    useEffect(() => {
-        axios.get('http://homestead.test/api/counter')
+    useEffect( () => {
+         axios.get('http://homestead.test/api/counter')
             .then(function (response) {
-                setCounter(response.data)
+                console.log(response.data)
+                setCounter(response.data.clicksTally)
             })
             .catch(function (error) {
                 console.log(error);
             })
     }, []);
     const incrementCount = () => {
-
         // Update state with incremented value
         setCounter(counter + 1);
+        // update backend value
+
+    };
+    const postCountValue = () => {
+        /*
+        - counter
+        */
+        axios.post('http://homestead.test/api/counter', {
+            counter,
+
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     };
     return (
             <div>
