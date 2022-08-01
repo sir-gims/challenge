5bf45602925e9a6a783e4f5cf33c0a02b6a29de3
Agim Bakiu <bakiuagim@gmail.com>
2022-08-01 23:53:43 +0200

    feat: installed Barryvdh\Cors & axios for client

--- zensmart-react/src/components/ClickMe/ClickMe.js
+++ zensmart-react/src/components/ClickMe/ClickMe.js
@@ -1,7 +1,25 @@
-import {useState} from "react";
-
+import {useState, useEffect} from "react";
+const axios = require('axios').default;
 function ClickMe() {
     let [counter, setCounter] = useState(0);
+    useEffect(() => {
+        axios.get('http://homestead.test/api/counter'
+        //     , {
+        //     params: {
+        //         ID: 12345
+        //     }
+        // }
+        )
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+            // .then(function () {
+            //     // always executed
+            // });
+    });
     const incrementCount = () => {
         // Update state with incremented value
         setCounter(counter + 1);
